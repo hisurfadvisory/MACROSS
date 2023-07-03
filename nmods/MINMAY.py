c901b4d7644bbe208092e2a9ac9c408c1f58ced4
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2023-07-02 19:39:30 -0500

    Update MINMAY.py

--- nmods/MINMAY.py
+++ nmods/MINMAY.py
@@ -3,7 +3,7 @@
 #_class User,demo script,Python 3,HiSurfAdvisory,2
 
 # By default, MACROSS always passes these vars to any python scripts it loads from the menu:
-# $USR, $vf19_DEFAULTPATH, $vf19_PYOPT, $vf19_numchk, $vf19_pylib, $vf19_TOOLSROOT
+# $USR, $vf19_DEFAULTPATH, $vf19_PYPOD, $vf19_numchk, $vf19_pylib, $vf19_TOOLSROOT
 # Python will also see the script name as sys.argv[0], so $USR will *ALWAYS* be sys.argv[1] 
 
 
@@ -12,13 +12,13 @@ import sys
 L = len(sys.argv)
 
 
+
 # If the user selected this script with the "h" option set, MACROSS sets the sys.argv[1] value
 # as "HELP" because they want to view the help-description page.
 if L > 2:
     USR = sys.argv[1]
     if USR == 'HELP':
         print("""
-
     This is a simple python script to demonstrate how
     MACROSS calls powershell and python tools inter-
     changeably.
@@ -32,7 +32,6 @@ if L > 2:
         and format HIKARU's query, using it to get info from
         another script called KÖNIG and return the results that
         HIKARU needed.
-
     Hit ENTER to continue.""")
         input()
         exit()
@@ -53,11 +52,11 @@ if L >= 7:
         ##  In order, they are:
         USR = sys.argv[1]               ## The logged-in user
         vf19_DEFAULTPATH = sys.argv[2]  ## USR's desktop filepath
-        vf19_PYOPT = sys.argv[3]        ## The encoded array of filepaths/URLs generated from extras.ps1
+        vf19_PYPOD = sys.argv[3]        ## The encoded array of filepaths/URLs generated from extras.ps1
         vf19_numchk = sys.argv[4]       ## The integer MACROSS uses for common math functions in all the scripts
         vf19_M = mc.makeM(vf19_numchk)  ## This function splits the numchk value into 6 digits you can use for mathing
         vf19_TOOLSROOT = sys.argv[6]    ## The path to the MACROSS folder
-        GBG = vf19_TOOLSROOT + '\\ncore\\py_classes\\garbage_io'  ## Path to the garbage I/O folder
+        GBG = sys.argv[5] + '\\garbage_io'  ## Path to the garbage I/O folder
         
         ## The psc function will pipe system commands into your powershell session
         mc.psc('cls')
@@ -112,7 +111,6 @@ uKVkOKVneKVmuKVkOKVneKVmuKVkOKVnSAg4pWa4pWQ4pWQ4pWQ4pWd4pWa4pWQ4pWdICAgICDilZril
 Z0gIOKVmuKVkOKVnSAgIOKVmuKVkOKVnSA='
     STR = mc.getThisPy(b,0)
     print('''
-
     ''')
     print(STR)
 
@@ -120,7 +118,6 @@ Z0gIOKVmuKVkOKVnSAgIOKVmuKVkOKVnSA='
 def theGoodStuff(Z1 = '',Z2 = None):
     splashPage()
     print('''
-
     Now, as of MACROSS version 3, the mcdefs python library (covered in a bit) doesn't
     have a smart way to share goodies like the powershell "collab" function (because my
     creator is lazy and not too bright).''')
@@ -128,7 +125,6 @@ def theGoodStuff(Z1 = '',Z2 = None):
         print('''
     I got your value --
                                ''',Z1,'''
-
     -- easy as getting an email, and I can sort-of make requests back to MACROSS just
     as easily, in fact I'm about to do just that by forwarding your filename to KÖNIG.
     But I will have to send my response to HIKARU's original query via snail-mail,
@@ -141,30 +137,33 @@ def theGoodStuff(Z1 = '',Z2 = None):
         next(1)
         mc.psc('cls')
     elif Z1 == '':
-        Z1 = input('''
+        print('''
     Now let's get some input from you. Give me a keyword to search for filenames
     with, preferably something you know can be found in your home folders (and it
-    can just be a partial filename):  ''')
+    can just be a partial filename).''')
+        while Z1 == '':
+            Z1 = input('''
+    Keyword: ''')
         Z1 = '\'' + Z1 + '\''
         
     print('''
     Let's continue with building a query to get KÖNIG on the launchpad:
-
+    
             konig = vf19_TOOLSROOT + '\\nmods\\KONIG.ps1'
             konig = 'powershell.exe ' + konig
-
+            
     I've already set that "vf19_TOOLSROOT" value in the background. Remember all
     those params/args that powershell sends over by default? This arg (the MACROSS
     root folder location) was stored in sys.argv[6]. So now python knows how to
     find KÖNIG. Now we can add the input you gave me to search for:
     
                     konig = konig + ''',Z1,'''
-
+    
     If you read KÖNIG's documentation, you'll see it needs several parameters passed
     to it if you try to call it without MACROSS, the first param being the value you
     supplied. Next it needs the name of the script calling it. If it's a python script
     (I am!), the name needs to begin with "py" (this is important as you'll see):
-
+    
                     konig = konig + 'pyMINMAY'
             
     ''')
@@ -174,35 +173,36 @@ def theGoodStuff(Z1 = '',Z2 = None):
     Finally, let's finish up by adding in the rest of KÖNIG's requirements: the location
     to search (I'm just going to set it to your local home folders):
         
-                usrhome = 'C:\\Users\\' + USR      ## USR was sent in sys.argv[1]
+                usrhome = 'C:\\Users\\' + USR      ## USR was sent to me in sys.argv[1]
                 konig = konig + usrhome
                 
     ...the location of the garbage_io folder (we'll cover that more in a bit):
-
-                gbg = vf19_TOOLSROOT + '\\ncore\\py_classes\\garbage_io'
+    
+            ## garbage_io is located in the same folder as the mcdefs library
+                gbg = sys.argv[5] + '\\garbage_io' 
                 konig = konig + gbg
-
+                
     ...and the location that MACROSS knows as "$vf19_DEFAULTPATH", and sent to me as
     sys.argv[2] (the current user's desktop, where KÖNIG writes its findings to):
-
+    
                 konig = konig + vf19_DEFAULTPATH + '-ErrorAction SilentlyContinue'
-
+                
     I also added the "ErrorAction" option at the end, because KÖNIG expects certain
     resources from MACROSS that it won't get from me, and we don't want to see all
     those error messages. Adding "py" to my name forces KÖNIG to make some adjustments
     to its functions. You can use another method in your scripts if you want.
-
+    
     Now, previously I left notes saying "more on that later"-- one for the mcdefs library,
     and another for MACROSS' garbage_io folder. The provided python library, mcdefs, is
     just a basic collection of other library resources that are used to offer some of the
     same common functions as MACROSS in powershell mode. The goal was to avoid having to
     do imports in python so often -- just import sys and mcdefs for the basics.
-
-    For this demo, I'm using the "psc" function in that library to execute a powershell
+    
+    For this demo, I'm using the "psc" function in mcdefs to execute a powershell
     script with the command I just finished building with your $PROTOCULTURE:
-
+    
                 mcdefs.psc(konig)
-
+                
     After KÖNIG finishes its mission and returns to python, I'll explain the "garbage_io".''')
     input('''
     Hit ENTER to launch KÖNIG!''')
@@ -223,19 +223,19 @@ def theGoodStuff(Z1 = '',Z2 = None):
     the garbage_io folder comes in. The mcdefs library will eventually have the ability to
     read results in straight from powershell to save you the hassle of coding it into every
     python script, but for now, MACROSS uses a text file.
-
+    
     Now, within the MACROSS root folder, inside the ncore subfolder, within the py_classes
     sub-sub-basement folder, there is a folder called garbage_io. It's not really for garbage,
     though. Its contents are very valuable! Part of the MACROSS framework is to build-in
     checks for your powershell scripts so that if they get called by a python script, they
     send their results to MACROSS' pyCross function. This will write whatever data your script
     needed to get, into an "eod" file that will be put in garbage_io.
-
+    
     If KÖNIG found anything for you, it should have written the location of its $RESULTFILE
     report, along with the number of results it got, to "konig.eod". The garbage_io location
     should still be valid as our variable "gbg" so, let's check using the mcdefs.dirfile()
     function...
-
+    
                 konfile = gbg + '\\konig.eod'
                 if mcdefs.dirfile(konfile,'isfile'):
                     chkresults = open(konfile,encoding='utf-8')
@@ -251,8 +251,7 @@ def theGoodStuff(Z1 = '',Z2 = None):
         chkresults = open(konfile,encoding='utf-8')
         konhits = chkresults.read()
         print('''
-    Hey! looks like we got the path to KÖNIG's $RESULTFILE, and the total hits!
-
+    Hey! It looks like we got the path to KÖNIG's $RESULTFILE, and the total hits for your search!
             ''')
         i = 0
         for line in konhits.split():
@@ -271,7 +270,7 @@ def theGoodStuff(Z1 = '',Z2 = None):
     work. It's no replacement for manually searching your security tools, but it can be a
     handy little utility to grab quick one-offs when deciding whether an event warrants
     deeper dives, or supplementing a smaller network that has less resources available for
-    cybersecurity beyond Active Directory and/or a few system logs.''')
+    cybersecurity beyond Active Directory cmdlets and/or a few system logs.''')
     if Z2 != None:
         Z2 = Z2 + '!'
         print('''
@@ -293,10 +292,10 @@ if USR == 'HELP':
     This is a python demo that has you input a filename to search for using the KONIG
     powershell script. It breaks down how the MACROSS framework makes it easy to have
     powershell and python tools work together seamlessly.
-
+    
     Running this script by itself explains how python scripts receive default information
     from MACROSS, and what those defaults are.
-
+    
     If you run the HIKARU demo script, it demonstrates how powershell and python tools
     can interact with each other and pass values back and forth.
     
@@ -323,28 +322,26 @@ if PROTOCULTURE:
     them.
     
     Okay, let me see... you have sent me exactly""",L - 1,"""arguments.
-
+    
     MACROSS will *always* send at least 6 args by default -- I used #5 to automatically import
     the MACROSS python library (and if you launch me from the MACROSS menu I talk a little
     bit more about that). But MACROSS has a built-in function called "collab" for powershell
     scripts, and it is designed to pass the additional values of $CALLER and $PROTOCULTURE,
     along with one additional argument if needed.
-
+    
     Because of that default behavior, your python scripts will always need the sys library
     (or at least its argv functionality) to be able to recognize all the values MACROSS sends
     over. In keeping with the framework's guidelines, I make a habit of keeping the same
     variable names. It's not really necessary, but it makes things easier to track. If you
     see me referencing "vf19_" variables later, it's because I converted a "$vf19_" variable
     from MACROSS using sys.argv.
-
+    
     Now, I'll use the 8th arg passed to me, which in MACROSS is $PROTOCULTURE, to ask a
     powershell script if it can find
     
     """,PROTOCULTURE,"""
-
     in any other directories, as a demonstration of using tools written in powershell asking
     for data from python and vice-versa. 
-
     """)
 
     next(0)
@@ -376,31 +373,28 @@ else:
     seamlessly loading python and powershell scripts to share and evaluate whatever an
     analyst may be investigating. (Named after Lynn Minmay from my favorite anime series,
     Macross).
-
+    
     At startup, MACROSS reads the Windows registry to see if Python3 is installed. If it
     is, the console will pull any .py files it finds in the nmods folder into the menu
     and make them available for use. If Python3 isn't installed, python scripts are ignored.
-
     By default, MACROSS always passes six arguments (in this order) to any python script it
     loads straight from the main menu:
-
+    
         [1] $USR = the logged in user
         [2] $vf19_DEFAULTPATH = the user's desktop path
-        [3] $vf19_PYOPT = the pythonized list of Base64 encoded defaults,
+        [3] $vf19_PYPOD = the pythonized list of Base64 encoded defaults,
             which in MACROSS is called $vf19_MPOD
         [4] $vf19_numchk = the hardcoded integer MACROSS uses for math functions
         [5] $vf19_pylib = the filepath to the 'mcdefs.py' file, which is the
             MACROSS python library (more on that later)
         [6] $vf19_TOOLSROOT = the MACROSS root folder, which contains the subfolders
             'nmods', 'resources' & 'ncore'
-
     """)
     next(1)
     mc.psc('cls')
     splashPage()
     
     print('''
-
     To begin this demo, we're going to launch a mission in the KÖNIG Monster powershell script.
     If you're not a geek, the KÖNIG Monster is a massive space shuttle-type craft that
     transforms into both a land-based rail-gun artillery monstrosity as well as a giant freaking
@@ -411,23 +405,17 @@ else:
     narrowly-targeted searches, not full-network scans). If it finds anything matching your search
     filters, it can automatically send those filepaths to the ELINTS script, which will scan each
     file for keywords you specify!
-
+    
     Being a cooperative part of MACROSS, KÖNIG can also accept requests from other scripts -- for
     example, MYLENE.ps1 is a tool that looks up Active-Directory information, especially focusing
     on newly-created accounts. Whenever MYLENE has a list of brand new user accounts, it forwards
     that list to KÖNIG which will automatically report on any and all files in those users' roaming
     profiles.
-
+    
     This python script is going to simulate you running some automated tool that found a filename
     of interest. Let's go!
-
     ''')
     next(1)
     mc.psc('cls')
     theGoodStuff()
-
-
-
-
-
 
