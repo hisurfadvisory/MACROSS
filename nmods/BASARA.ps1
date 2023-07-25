b003d20d7577cb83ab6c621131162aa108b1969c
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2023-07-24 19:30:52 -0500

    Update BASARA.ps1

--- nmods/BASARA.ps1
+++ nmods/BASARA.ps1
@@ -1,10 +1,10 @@
-﻿#_sdf1 Code-signing & Cert inspection
+#_sdf1 Code-signing & Cert inspection
 #_ver 1.5
 #_class User,Digital_Certs,Powershell,HiSurfAdvisory,0
 
 ## Watch for alt filenames requested from python scripts via 'collab'
 param(
-    [string]$dyrl_hik_NOPROTO
+    [string]$dyrl_bas_NOPROTO
 )
 
 function splashPage(){
@@ -44,18 +44,17 @@ if( $HELP ){
    Write-Host -f YELLOW $_
  }
  Write-Host -f YELLOW "
-     Code-signing is enforced for security. If you have a signing certificate,
+     Code-signing may be enforced for security. If you have a signing certificate,
      you can use this tool to quickly sign your new scripts so they will be
      allowed to run, or re-sign expired/updated code.
-
-     Simply put your script in the \nmods folder, or let HIKARU prompt you
+     Simply put your script in the \nmods folder, or let BASARA prompt you
      for the file location.
  
-     If you run HIKARU with the 's' option (ex '1s'), it disables the code-
+     If you run BASARA with the 's' option (ex '1s'), it disables the code-
      signing function and instead lets you inspect the details for a file's
      digital signature.
 
-     Press ENTER to continue.
+     Hit ENTER to continue.
  "
  Read-Host
  Return
@@ -78,20 +77,19 @@ splashPage
 ## Alternate mode to inspect files instead of signing them
 if( $vf19_OPT1 ){
     Write-Host ''
-    while( $dyrl_hik_Z -ne 'q' ){
+    while( $dyrl_bas_Z -ne 'q' ){
         Write-Host '
-
         '
         Write-Host -f GREEN " Type 'f' to select a file for inspection, or " -NoNewline;
         Write-Host -f YELLOW 'q' -NoNewline;
         Write-Host -f GREEN ' to quit: ' -NoNewline;
-        $dyrl_hik_Z = Read-Host
+        $dyrl_bas_Z = Read-Host
 
-        if( $dyrl_hik_Z -ne 'q' ){
-            $dyrl_hik_Z = getFile
-            if($dyrl_hik_Z -ne ''){
-                peekCert $dyrl_hik_Z
-                Clear-Variable dyrl_hik_Z
+        if( $dyrl_bas_Z -ne 'q' ){
+            $dyrl_bas_Z = getFile
+            if($dyrl_bas_Z -ne ''){
+                peekCert $dyrl_bas_Z
+                Clear-Variable dyrl_bas_Z
             }
         }
     }
@@ -99,21 +97,20 @@ if( $vf19_OPT1 ){
     Return
 }
 if( $CALLER ){
-    if( $dyrl_hik_NOPROTO ){
-        $dyrl_hik_v = $dyrl_hik_NOPROTO
+    if( $dyrl_bas_NOPROTO ){
+        $dyrl_bas_v = $dyrl_bas_NOPROTO
     }
     elseif($PROTOCULTURE){
-        $dyrl_hik_v = $PROTOCULTURE
+        $dyrl_bas_v = $PROTOCULTURE
     }
-    if($dyrl_hik_v){
-    peekCert $dyrl_hik_v
+    if($dyrl_bas_v){
+    peekCert $dyrl_bas_v
     }
     else{
         Write-Host -f CYAN '
     ERROR! No filename given...'
     }
     Write-Host -f GREEN "
-
     Hit ENTER to return to $CALLER.
     "
     Read-Host
@@ -145,7 +142,7 @@ function unSign($spath){
                 "
         Write-Host -f GREEN " Hit ENTER to exit and prevent the apocalypse..."
         Read-Host
-        Remove-Variable dyrl_hik_* -Scope Global
+        Remove-Variable dyrl_bas_* -Scope Global
         if( $CALLHOLD ){
             Return
         }
@@ -161,7 +158,6 @@ function unSign($spath){
 function certSign($spath,$cert){
     Set-AuthenticodeSignature -FilePath $spath -Certificate $cert
     Write-Host "
-
     "
     if( (Get-AuthenticodeSignature -FilePath $spath).status -eq 'Valid' ){
         Write-Host -f GREEN " Success! Do you need to sign another (" -NoNewline;
@@ -184,14 +180,14 @@ function certSign($spath,$cert){
 
 ## Default vars
 $ErrorActionPreference = 'SilentlyContinue'
-$dyrl_hik_DEFPATH = "$PSScriptRoot\.\"  ## This script can be used with or without MACROSS
+$dyrl_bas_DEFPATH = "$PSScriptRoot\.\"  ## This script can be used with or without MACROSS
 
 ## Valid inputs
-$dyrl_hik_FNMATCH = [regex]"^[a-zA-Z0-9_.]*\.[a-z0-9]+$"
-$dyrl_hik_YN = [regex]"^(y|n)"
+$dyrl_bas_FNMATCH = [regex]"^[a-zA-Z0-9_.]*\.[a-z0-9]+$"
+$dyrl_bas_YN = [regex]"^(y|n)"
 
 ## Let's kick it off!
-$dyrl_hik_Z = $null
+$dyrl_bas_Z = $null
 
 
 splashPage
@@ -200,81 +196,81 @@ splashPage
 #################
 ## MAIN SCRIPT ##
 #################
-while ( $dyrl_hik_Z -ne 'c' ){
+while ( $dyrl_bas_Z -ne 'c' ){
     Write-Host -f GREEN "
-    Write the script name if it is in the the same folder as HIKARU, OR hit ENTER to
+    Write the script name if it is in the the same folder as BASARA, OR hit ENTER to
     select your file, OR type 'c' to cancel:
         
     >  " -NoNewline;
-    $dyrl_hik_Z = Read-Host
+    $dyrl_bas_Z = Read-Host
 
-    if( $dyrl_hik_Z -ne 'c' ){
-        if( $dyrl_hik_Z -eq '' ){
-            $dyrl_hik_SPATH = getFile
-            if($dyrl_hik_SPATH = ''){
+    if( $dyrl_bas_Z -ne 'c' ){
+        if( $dyrl_bas_Z -eq '' ){
+            $dyrl_bas_SPATH = getFile
+            if($dyrl_bas_SPATH = ''){
                 Write-Host -f CYAN '    Action cancelled...'
                 slp 2
                 Exit
             }
         }
-        elseif( $dyrl_hik_Z -Match $dyrl_hik_FNMATCH ){
-            $dyrl_hik_SPATH = "$dyrl_hik_DEFPATH\$dyrl_hik_Z"
+        elseif( $dyrl_bas_Z -Match $dyrl_bas_FNMATCH ){
+            $dyrl_bas_SPATH = "$dyrl_bas_DEFPATH\$dyrl_bas_Z"
         }
     }
     else{
         Exit
     }
 
-    if( (Test-Path -Path "$dyrl_hik_SPATH") ){
+    if( (Test-Path -Path "$dyrl_bas_SPATH") ){
         ## Get the signing cert!
-        $dyrl_hik_CERT = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert
+        $dyrl_bas_CERT = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert
 
 
         ## Check to see if code is already signed
-        if( Get-AuthenticodeSignature -FilePath $dyrl_hik_SPATH | where{$_.Status -eq 'Valid'} ){
-            $dyrl_hik_BSURE = $true
-            while( $dyrl_hik_Z -notMatch $dyrl_hik_YN ){
+        if( Get-AuthenticodeSignature -FilePath $dyrl_bas_SPATH | where{$_.Status -eq 'Valid'} ){
+            $dyrl_bas_BSURE = $true
+            while( $dyrl_bas_Z -notMatch $dyrl_bas_YN ){
                 Write-Host ""
                 Write-Host -f CYAN " Warning! " -NoNewline;
                     Write-Host -f GREEN "The file you entered is already signed!
                     "
                 Write-Host -f GREEN " Overwrite the existing cert (y/n)?  " -NoNewline;
-                $dyrl_hik_Z = Read-Host
-                $dyrl_hik_A = $true
+                $dyrl_bas_Z = Read-Host
+                $dyrl_bas_A = $true
             }
         }
         else{
-            $dyrl_hik_Z = 'y'
+            $dyrl_bas_Z = 'y'
         }
 
 
-        if( $dyrl_hik_Z -eq 'y' ){
-            if( $dyrl_hik_BSURE ){
-                unSign $dyrl_hik_SPATH
+        if( $dyrl_bas_Z -eq 'y' ){
+            if( $dyrl_bas_BSURE ){
+                unSign $dyrl_bas_SPATH
             }
 
-            certSign $dyrl_hik_SPATH $dyrl_hik_CERT
-            $dyrl_hik_Z = Read-Host
+            certSign $dyrl_bas_SPATH $dyrl_bas_CERT
+            $dyrl_bas_Z = Read-Host
 
         }
 
 
-        if( ($dyrl_hik_Z -eq 'y') -or ($dyrl_hik_A) ){
-            Clear-Variable -Force dyrl_hik_SPATH
-            Clear-Variable -Force dyrl_hik_CERT
-            Clear-Variable -Force dyrl_hik_Z
-            Remove-Variable dyrl_hik_A -Scope Global
+        if( ($dyrl_bas_Z -eq 'y') -or ($dyrl_bas_A) ){
+            Clear-Variable -Force dyrl_bas_SPATH
+            Clear-Variable -Force dyrl_bas_CERT
+            Clear-Variable -Force dyrl_bas_Z
+            Remove-Variable dyrl_bas_A -Scope Global
         }
         else{
-            Remove-Variable dyrl_hik_* -Scope Global
+            Remove-Variable dyrl_bas_* -Scope Global
             Exit
         }
     }
     else{
         Write-Host -f CYAN ' File not found!
         '
-        Clear-Variable -Force dyrl_hik_SPATH
-        Clear-Variable -Force dyrl_hik_CERT
-        Clear-Variable -Force dyrl_hik_Z
+        Clear-Variable -Force dyrl_bas_SPATH
+        Clear-Variable -Force dyrl_bas_CERT
+        Clear-Variable -Force dyrl_bas_Z
     }
 }
