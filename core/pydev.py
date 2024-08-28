f3ec8f75f42d39a112b1fb6075bfa92f0c2c70bc
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2024-08-28 08:41:51 -0500

    Update pydev.py

--- core/pydev.py
+++ core/pydev.py
@@ -16,8 +16,8 @@ with open(EOD) as E:
 IN = None
 mc.w('''
     The MACROSS mcdefs library has been imported as "mc", all the standard MACROSS
-    globals have been populated (sys.argv 0 - 7), and the LATTS.eod file can be
-    referenced using the array LATT.
+    globals have been populated. The LATTS.eod file has been converted into a
+    dictionary, "LATTS".
      
     Just type "q" to quit back to the MACROSS menu.
 ''','g')
