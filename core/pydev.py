5b3a888f4c477a553d47b80efa12e9aea2113d22
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2024-08-27 20:16:14 -0500

    Update pydev.py

--- core/pydev.py
+++ core/pydev.py
@@ -3,23 +3,29 @@
 #_class user,debugging,python,HiSurfAdvisory,0,none
 
 import sys
-MCDEF = sys.argv[6]
+#MCDEF = sys.argv[6]
+MCDEF = sys.argv[1]
 sys.path.insert(0,MCDEF)
 import mcdefs as mc
+import json
 
+EOD = MCDEF + "\\garbage_io\\LATTS.eod"
+with open(EOD) as E:
+    LATT = json.load(E)
 
 IN = None
 mc.w('''
-    The MACROSS mcdefs library has been imported as "mc" and all the standard MACROSS
-    globals have been populated (sys.argv 0 - 7).
+    The MACROSS mcdefs library has been imported as "mc", all the standard MACROSS
+    globals have been populated (sys.argv 0 - 7), and the LATTS.eod file can be
+    referenced using the array LATT.
+     
+    Just type "q" to quit back to the MACROSS menu.
 ''','g')
-while IN != 'q':
-    IN = input('Enter your test commands, or "q" to quit: ')
-    if IN != 'q':
-        try:
-            exec(IN)
-        except:
-            mc.w('no good!','r')
+while True:
+    mc.w('MACROSS> ','g',i=True)
+    Z = input()
+    if Z == 'q':
+        exit()
+    else:
+        exec(Z)
 
-
-exit()
