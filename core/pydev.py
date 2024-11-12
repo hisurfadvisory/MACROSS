1b37b3fc55527d7a75418d13a56d0d0ab9320b5e
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2024-11-12 17:58:46 -0600

    Update pydev.py

--- core/pydev.py
+++ core/pydev.py
@@ -2,30 +2,29 @@
 #_ver 1
 #_class user,debugging,python,HiSurfAdvisory,0,none
 
-import sys
-#MCDEF = sys.argv[6]
-MCDEF = sys.argv[1]
-sys.path.insert(0,MCDEF)
-import mcdefs as mc
-import json
+import valkyrie as vk
 
-EOD = MCDEF + "\\garbage_io\\LATTS.eod"
-with open(EOD) as E:
-    LATT = json.load(E)
 
 IN = None
-mc.w('''
-    The MACROSS mcdefs library has been imported as "mc", all the standard MACROSS
-    globals have been populated. The LATTS.eod file has been converted into a
-    dictionary, "LATT".
+vk.screenResults("y~UTILITIES LIST","availableTypes, screenResults, collab, dS, drfl, errLog, getFile, getThis, help, osrm, osys, psc, psp, rgx, slp, ts, w")
+vk.screenResults('Type "vk.help()" for valkyrie descriptions, or help() with any of the above valkyrie functions to view their help messages. Example: help(vk.collab)')
+vk.screenResults()
+vk.w('''
+    The MACROSS valkyrie module has been imported as "vk", and all the standard 
+    MACROSS globals have been populated. The powershell hashtable $vf19_LATTS
+    has been converted into the python dictionary vk.LATTS, so you can test the 
+    vk.availableTypes() and vk.collab() functions.
      
     Just type "q" to quit back to the MACROSS menu.
 ''','g')
 while True:
-    mc.w('MACROSS> ','g',i=True)
+    vk.w('MACROSS> ','g',i=True)
     Z = input()
     if Z == 'q':
         exit()
     else:
-        exec(Z)
+        try:
+            exec(Z)
+        except:
+            vk.w('  Could not execute that command!','c')
 
