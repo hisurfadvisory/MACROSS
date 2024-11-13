cba269f406f69f0ea588382aa900bf967058a48f
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2024-11-12 18:44:56 -0600

    Update MINMAY.py

--- modules/MINMAY.py
+++ modules/MINMAY.py
@@ -2,39 +2,29 @@
 #_ver 0.2
 #_class 0,user,demo script,python,HiSurfAdvisory,2,onscreen
 
-# By default, MACROSS always passes these vars to any python scripts it loads from the menu:
-# $USR, $pyATTS, $vf19_DTOP, $vf19_PYPOD, $vf19_numchk, $vf19_pylib, $vf19_TOOLSROOT
-# Python will also see the script name as sys.argv[0], so $USR will *ALWAYS* be sys.argv[1]
-# See lines 46-62 for the values in these variables.
-
-
-## Interacting with MACROSS requires the sys library's argv and path functions
-from sys import argv,path
+from sys import argv
+from json import loads
+import valkyrie as vk
 L = len(argv)
-
+spiritia = False
     
-# MACROSS sends 7 args by default; the 6th is always the filepath to the mcdefs library
-if L >= 2:
-    mpath = argv[1]
-    path.insert(0,mpath)  ## modify the sys path to include the py_classes folder
-    import mcdefs as mc
-    mc.psc('cls')
+## MACROSS can send a single optional arg, if your script is coded to accept one
+if L == 2:
+    spiritia = argv[1]
+    
+    
+vk.psc('cls')
 
-## When called via the collab() function, python scripts can receive a second arg if a value
-## other than PROTOCULTURE is needed.
-if L == 3:
-    OPT = argv[2]
-
-## If the user selected this script with the "help" option, the mcdefs library will set the
+## If the user selected this script with the "help" option, the valkyrie module will set the
 ## HELP value to T so that your help/description message can be displayed.
-if mc.HELP == 'T':
+if vk.HELP:
     print("""
     This is a simple python script to demonstrate how MACROSS calls powershell and python 
     tools interchangeably.
     
     1. Running MINMAY by itself will demonstrate reading
         MACROSS' default values into your script and using them
-        to import the custom 'mcdefs' python library.
+        to import the custom 'valkyrie' python library.
     
     2. If you run the HIKARU demo script, it will demonstrate
         sending parameters/arguments to MINMAY, which will process
@@ -61,89 +51,69 @@ ilZrilZDilZ0g4paI4paI4pWR4paI4paI4pWR4paI4paI4pWRIOKVmuKWiOKWiOKWiOKWiOKVkeKWiOK
 DilZ0g4paI4paI4pWR4paI4paI4pWRICDilojilojilZEgICDilojilojilZEgCiAgICAgICDilZrilZDilZ0gICAgIOKVm\
 uKVkOKVneKVmuKVkOKVneKVmuKVkOKVnSAg4pWa4pWQ4pWQ4pWQ4pWd4pWa4pWQ4pWdICAgICDilZrilZDilZ3ilZrilZDil\
 Z0gIOKVmuKVkOKVnSAgIOKVmuKVkOKVnSA='
-    STR = mc.getThis(b)
+    STR = vk.getThis(b)
     print("\n")
     if alt:
-        mc.w(STR,'m')
+        vk.w(STR,'m')
     else:
-        mc.w(STR,'y')
+        vk.w(STR,'y')
     print("\n\n")
 
-test = False
 
-## This script knows ahead of time which script it might need to call
-TOOL = "GUBABA.ps1"
 
-if mc.PROTOCULTURE != 'None':
-    splashPage(alt=True)
-    CALLER = mc.CALLER
-    mc.w('CALLER: ','g',i=True); mc.w(CALLER,'y')
-    mc.w('PROTOCULTURE: ','g',i=True); mc.w(mc.PROTOCULTURE,'y')
-    mc.w("\n\nHIKARU used the collab function to send your value to MINMAY.\n",'m')
-    test = mc.collab(TOOL,'MINMAY',mc.PROTOCULTURE)
-    mc.slp(2)
+## Use availableTypes to generate lists of relevant scripts, regardless of files
+## being added/removed from your core folder
+TOOL = vk.availableTypes('event id',la='powershell')[0]
+
+if vk.PROTOCULTURE:
+    ## In this section, MINMAY is looking at the calling script's MACROSS class,
+    ## specifically the ".valtype" value. Using the MACROSS class attributes lets 
+    ## you write your tools to respond appropriately no matter which script is 
+    ## calling yours. MINMAY will only act on queries from scripts with the .valtype 
+    ## "demo script" and the .lang "powershell".
+    if vk.CALLER:
+        vtype = vk.LATTS[vk.CALLER].valtype
+        vlang = vk.LATTS[vk.CALLER].lang
+        if vtype == 'demo script' and vlang == 'powershell':
+            splashPage(alt=True)
+            vk.w('CALLER: ','g',i=True); vk.w(vk.CALLER,'y')
+        else:
+            exit()
+    
+    vk.w('PROTOCULTURE: ','g',i=True); vk.w(vk.PROTOCULTURE,'y')
+    print("\n\n")
+    vk.w(vk.CALLER+" used the collab function to send your value to MINMAY.\n",'m')
+    test = vk.collab(TOOL,vk.CALLER,vk.PROTOCULTURE)
+    vk.slp(2)
 else:
     splashPage()
-    CALLER = False
-    mc.w('Enter a search term or ID related to Windows events to see if GUBABA can find it: ','g',i=True)
+    vk.w('''Enter a search term or ID related to Windows events to see if GUBABA 
+can find it: ''','g',i=True)
     Z = input()
-    test = mc.collab(TOOL,'MINMAY',Z)
+    test = vk.collab(TOOL,'MINMAY',Z)
 
+if test:
+    vk.w("MINMAY has processed your search via "+TOOL+".\n\n",'g')
 
-if test != 'False':
-    if CALLER != False:
-        mc.w("MINMAY has processed your search via GUBABA.\n\n",'m')
-        exit()
     
     ## The powershell GUBABA script responds with hashtables; MACROSS has a powershell function called
-    ## pyCross that writes the $PROTOCULTURE value to the PROTOCULTURE.eod file within the core\py_classes\
-    ## garbage_io folder. The pyCross function automatically converts hashtables and lists into strings for 
-    ## python that you will need to convert & parse in your tools.
-    if mc.rgx(".*@@.*",test):
-        demoDict = {}
-        demoList = test.split("@@")
-        for i in demoList:
-            kv = i.split(':')[0]
-            vk = i.split(':')[1]
-            demoDict[kv] = vk
-        ## The mcdefs library contains many of MACROSS' powershell functions, including  w() and screenResults() 
-        ## that you can use to format your outputs onscreen.
-        mc.w('GUBABA RESULTS USING w()','m')
-        for d in demoDict:
-            mc.w(d + ': ','g',i=True); mc.w(demoDict[d],'y')
-        print("""
-        """)
-        del d
-        input("""Hit ENTER to continue.
-        """)
-
-        
-        mc.w('GUBABA RESULTS USING screenResults()','m')
-        for d in demoDict:
-            cc = "c~" + d
-            dd = demoDict[d]
-            mc.screenResults(cc,dd)
-        mc.screenResults()
-        print("""
-        """)
+    ## pyCross that writes the $PROTOCULTURE value to the PROTOCULTURE.eod file within the core\macross_py\
+    ## garbage_io folder. The valkyrie.collab() function reads/writes this file to send and retrieve
+    ## PROTOCULTURE values for your scripts.
+    for i in test.keys():
+        t = str(test[i][0])
+        v = str(test[i][1])
+        vk.screenResults('y~'+t+'-'+'ID '+i,v)
+    vk.screenResults()
+    if vk.CALLER:
+        vk.w("\nHit ENTER to return to "+vk.CALLER,'g')
     else:
-        mc.w('GUBABA RESULTS USING mcdefs.w():','m')
-        mc.w(test,'g')
-        print("""
-        """)
-        input("""Hit ENTER to continue.
-        """)
-
-        
-        mc.w('GUBABA RESULTS USING mcdefs.screenResults():','m')
-        mc.screenResults(test)
-        mc.screenResults()
-        print("""
-        """)
-
-    mc.w("\nHit ENTER to quit back to the menu.",'g')
+        vk.w("\nHit ENTER to quit back to the menu.",'g')
 else:
-    mc.w("\nNo results. Hit ENTER to quit back to the menu.",'c')
+    if vk.CALLER:
+        vk.w("\nHit ENTER to return to "+vk.CALLER,'g')
+    else:
+        vk.w("\nNo results. Hit ENTER to quit back to the menu.",'c')
 
 
 
