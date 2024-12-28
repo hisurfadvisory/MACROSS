65f14d007d7816e97c35bc05c076c94c16e2c1d4
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2024-12-28 10:20:59 -0600

    Update and rename MINMAY.py to MISA.py

--- modules/MISA.py
+++ modules/MISA.py
@@ -6,7 +6,7 @@ from sys import argv
 from json import loads
 import valkyrie as vk
 L = len(argv)
-spiritia = False  ## "spiritia" is used as an alt arg/param in addition to or instead of PROTOCULTURE.
+spiritia = False  ## "spiritia" is used as an alt arg/param in addition to, or instead of, PROTOCULTURE.
     
 ## MACROSS can send a single optional arg, if your script is coded to accept one
 if L == 2:
@@ -14,21 +14,22 @@ if L == 2:
     
 
 ## The valkyrie.psc() function is just "os.system()"
-vk.psc('cls')  
+## Using arg "cc" executes commands, while using "cr" gives you back the results as usable data.
+vk.psc(cc='cls')  
 
 ## If the user selected this script with the "help" option, the valkyrie module will set the
-## HELP value to T so that your help/description message can be displayed.
+## HELP value to True so that your help/description message can be displayed.
 if vk.HELP:
     print("""
     This is a simple python script to demonstrate how MACROSS calls powershell and python 
     tools interchangeably.
     
-    1. Running MINMAY by itself will demonstrate reading
+    1. Running MISA by itself will demonstrate reading
         MACROSS' default values into your script and using them
         to import the custom 'valkyrie' python library.
     
     2. If you run the HIKARU demo script, it will demonstrate
-        sending parameters/arguments to MINMAY, which will process
+        sending parameters/arguments to MISA, which will process
         and format HIKARU's query, using it to get info from
         another script called GUBABA and return the results that
         HIKARU needed.
@@ -39,19 +40,16 @@ if vk.HELP:
 
 
 def splashPage(alt=False):
-    b = 'ICAgICAgIOKWiOKWiOKWiOKVlyAgIOKWiOKWiOKWiOKVl+KWiOKWiOKVl+KWiOKWiOKWiOKVlyAgIOKWiO\
-KWiOKVl+KWiOKWiOKWiOKVlyAgIOKWiOKWiOKWiOKVlyDilojilojilojilojilojilZcg4paI4paI4pWXICAg4paI4p\
-aI4pWXCiAgICAgICDilojilojilojilojilZcg4paI4paI4paI4paI4pWR4paI4paI4pWR4paI4paI4paI4paI4pWXIC\
-DilojilojilZHilojilojilojilojilZcg4paI4paI4paI4paI4pWR4paI4paI4pWU4pWQ4pWQ4paI4paI4pWX4pWa4p\
-aI4paI4pWXIOKWiOKWiOKVlOKVnQogICAgICAg4paI4paI4pWU4paI4paI4paI4paI4pWU4paI4paI4pWR4paI4paI4pW\
-R4paI4paI4pWU4paI4paI4pWXIOKWiOKWiOKVkeKWiOKWiOKVlOKWiOKWiOKWiOKWiOKVlOKWiOKWiOKVkeKWiOKWiOKW\
-iOKWiOKWiOKWiOKWiOKVkSDilZrilojilojilojilojilZTilZ0gCiAgICAgICDilojilojilZHilZrilojilojilZTilZ\
-3ilojilojilZHilojilojilZHilojilojilZHilZrilojilojilZfilojilojilZHilojilojilZHilZrilojilojilZTi\
-lZ3ilojilojilZHilojilojilZTilZDilZDilojilojilZEgIOKVmuKWiOKWiOKVlOKVnSAKICAgICAgIOKWiOKWiOKVkSD\
-ilZrilZDilZ0g4paI4paI4pWR4paI4paI4pWR4paI4paI4pWRIOKVmuKWiOKWiOKWiOKWiOKVkeKWiOKWiOKVkSDilZrilZ\
-DilZ0g4paI4paI4pWR4paI4paI4pWRICDilojilojilZEgICDilojilojilZEgCiAgICAgICDilZrilZDilZ0gICAgIOKVm\
-uKVkOKVneKVmuKVkOKVneKVmuKVkOKVnSAg4pWa4pWQ4pWQ4pWQ4pWd4pWa4pWQ4pWdICAgICDilZrilZDilZ3ilZrilZDil\
-Z0gIOKVmuKVkOKVnSAgIOKVmuKVkOKVnSA='
+    b = 'ICAgICAgICDilojilojilojilZcgICDilojilojilojilZfilojilojilZfilojilojilojilojilojil\
+        ojilojilZcg4paI4paI4paI4paI4paI4pWXIAogICAgICAgIOKWiOKWiOKWiOKWiOKVlyDilojilojiloj\
+        ilojilZHilojilojilZHilojilojilZTilZDilZDilZDilZDilZ3ilojilojilZTilZDilZDilojilojil\
+        ZcKICAgICAgICDilojilojilZTilojilojilojilojilZTilojilojilZHilojilojilZHilojilojiloj\
+        ilojilojilojilojilZfilojilojilojilojilojilojilojilZEKICAgICAgICDilojilojilZHilZril\
+        ojilojilZTilZ3ilojilojilZHilojilojilZHilZrilZDilZDilZDilZDilojilojilZHilojilojilZT\
+        ilZDilZDilojilojilZEKICAgICAgICDilojilojilZEg4pWa4pWQ4pWdIOKWiOKWiOKVkeKWiOKWiOKVk\
+        eKWiOKWiOKWiOKWiOKWiOKWiOKWiOKVkeKWiOKWiOKVkSAg4paI4paI4pWRCiAgICAgICAg4pWa4pWQ4pW\
+        dICAgICDilZrilZDilZ3ilZrilZDilZ3ilZrilZDilZDilZDilZDilZDilZDilZ3ilZrilZDilZ0gIOKVm\
+        uKVkOKVnQo='
     STR = vk.getThis(b)
     print("\n")
     if alt:
@@ -62,17 +60,20 @@ Z0gIOKVmuKVkOKVnSAgIOKVmuKVkOKVnSA='
 
 
 
-## Use availableTypes() to generate lists of relevant scripts, regardless of files
-## being added/removed from your core folder
+## Use availableTypes() to generate lists of relevant scripts you can forward data to, 
+## regardless of scripts being added/removed from your modules folder. If you look at 
+## GUBABA's .valtype, it is "windows event id lookup". You can search for exact or 
+## partial-matching valtypes. The "la" arg is specifying which language we require from
+## potential collab scripts.
 TOOL = vk.availableTypes('event id',la='powershell')[0]
 
 ## The standard rule for MACROSS scripts is that they automatically act on the
 ## global value PROTOCULTURE, if it exists.
 if vk.PROTOCULTURE:
-    ## In this section, MINMAY is looking at the CALLER script's MACROSS class,
-    ## specifically the ".valtype" value. Using the MACROSS class attributes lets 
+    ## In this section, MISA is looking at the CALLER script's macross class,
+    ## specifically the ".valtype" value. Using the macross class attributes lets 
     ## you write your tools to respond appropriately no matter which script is 
-    ## calling yours. MINMAY will only reply to queries from scripts with the .valtype 
+    ## calling yours. MISA will only reply to queries from scripts with the .valtype 
     ## "demo script" and the .lang "powershell".
     if vk.CALLER:
         vtype = vk.LATTS[vk.CALLER].valtype
@@ -85,7 +86,7 @@ if vk.PROTOCULTURE:
     
     vk.w('PROTOCULTURE: ','g',i=True); vk.w(vk.PROTOCULTURE,'y')
     print("\n\n")
-    vk.w(vk.CALLER+" used the collab function to send your value to MINMAY.\n",'m')
+    vk.w(vk.CALLER+" used the collab function to send your value to MISA.\n",'m')
     test = vk.collab(TOOL,vk.CALLER,vk.PROTOCULTURE)
     vk.slp(2)
 else:
@@ -93,19 +94,19 @@ else:
     vk.w('''Enter a search term or ID related to Windows events to see if GUBABA 
 can find it: ''','g',i=True)
     Z = input()
-    test = vk.collab(TOOL,'MINMAY',Z)
+    test = vk.collab(TOOL,'MISA',Z)
 
 if test:
-    vk.w("MINMAY has processed your search via "+TOOL+".\n\n",'g')
+    vk.w("MISA has processed your search via "+TOOL+".\n\n",'g')
 
     
     ## The powershell GUBABA script responds with hashtables; MACROSS has a powershell function called
-    ## pyCross that writes the $PROTOCULTURE value to the PROTOCULTURE.eod file within the core\macross_py\
-    ## garbage_io folder. The valkyrie.collab() function reads/writes this file to send and retrieve
-    ## PROTOCULTURE values for your scripts.
+    ## pyCross that your scripts can use to write a $PROTOCULTURE value to the PROTOCULTURE.eod file 
+    ## within the core\macross_py\garbage_io folder. The python valkyrie.collab() function uses this 
+    ## file to send and retrieve PROTOCULTURE values back and forth between python and powershell.
     for i in test.keys():
         t = str(test[i][0])
-        v = str(test[i][1])
+        v = str(test[i])
         ## Both the powershell and python modules contain useful utilities you can use, such as
         ## screenResults(), which formats large blocks of text into columns
         vk.screenResults('y~'+t+'-'+'ID '+i,v)
