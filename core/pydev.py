6c2849d7007eeae025d2f4fa1afb61d4fc98d1bd
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2025-01-11 07:35:34 -0600

    Update pydev.py

--- core/pydev.py
+++ core/pydev.py
@@ -4,27 +4,31 @@
 
 import valkyrie as vk
 
+def main():
+    IN = None
+    vk.screenResults("y~UTILITIES LIST","availableTypes, screenResults, collab, dS, drfl, errLog, getFile, getThis, help, osrm, osys, psc, psp, rgx, slp, ts, w")
+    vk.screenResults('Type "vk.help()" for valkyrie descriptions, or help() with any of the above valkyrie functions to view their help messages. Example: help(vk.collab)')
+    vk.screenResults()
+    vk.w('''
+        The MACROSS valkyrie module has been imported as "vk", and all the standard 
+        MACROSS globals have been populated. The powershell hashtable $vf19_LATTS
+        has been converted into the python dictionary vk.LATTS, so you can test the 
+        vk.availableTypes() and vk.collab() functions.
+        
+        Just type "q" to quit back to the MACROSS menu.
+    ''','g')
+    while True:
+        vk.w('MACROSS> ','g',i=True)
+        Z = input()
+        if Z == 'q':
+            exit()
+        else:
+            try:
+                exec(Z)
+            except Exception as e:
+                print(e)
 
-IN = None
-vk.screenResults("y~UTILITIES LIST","availableTypes, screenResults, collab, dS, drfl, errLog, getFile, getThis, help, osrm, osys, psc, psp, rgx, slp, ts, w")
-vk.screenResults('Type "vk.help()" for valkyrie descriptions, or help() with any of the above valkyrie functions to view their help messages. Example: help(vk.collab)')
-vk.screenResults()
-vk.w('''
-    The MACROSS valkyrie module has been imported as "vk", and all the standard 
-    MACROSS globals have been populated. The powershell hashtable $vf19_LATTS
-    has been converted into the python dictionary vk.LATTS, so you can test the 
-    vk.availableTypes() and vk.collab() functions.
-     
-    Just type "q" to quit back to the MACROSS menu.
-''','g')
-while True:
-    vk.w('MACROSS> ','g',i=True)
-    Z = input()
-    if Z == 'q':
-        exit()
-    else:
-        try:
-            exec(Z)
-        except Exception as e:
-            print(e)
 
+
+if __name__ == "__main__":
+    main()
