6b7037f8b008c4d8a2e6ed4fa1fea0cdbd8fdac7
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2025-02-23 14:12:29 -0600

    Update __init__.py

--- core/macross_py/__init__.py
+++ core/macross_py/__init__.py
@@ -1,1 +1,37 @@
-from macross_py import valkyrie
+"""
+The valkyrie library is a set of MACROSS functions converted from powershell to python.
+
+version 1.5
+
+https://github.com/hisurfadvisory/MACROSS
+
+"""
+from .valkyrie import (
+    help,
+    w,
+    slp,
+    errLog,
+    dS,
+    rgx,
+    psc,
+    drfl,
+    availableTypes,
+    collab,
+    getFile,
+    screenResults,
+    getThis,
+    PROTOCULTURE,
+    CALLER,
+    HELP,
+    USR,
+    GBIO,
+    RSRC,
+    DTOP,
+    TOOLSROOT,
+    TOOLSDIR,
+    LOGS,
+    M_,
+    N_,
+    ROBOTECH,
+    MPOD
+)
