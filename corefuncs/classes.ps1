60582316e8d585845f38461953ecd6c51c3bed6c
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2026-06-24 17:10:04 -0500

    Update classes.ps1

--- corefuncs/classes.ps1
+++ corefuncs/classes.ps1
@@ -1,53 +1,41 @@
 ## Custom classes for MACROSS
 
 
-## Classify MACROSS diamonds:
-## The 3rd line in every script in the \diamonds folder must be the classifier:
-##
-##      #_class $priv,$access,$valtype,$lang,$author,$evalmax,$rtype
-##
-## -Users get access to their intended diamond; admin diamonds won't be copied to user profiles,
-##      tier 1 diamonds won't get copied to tier 3 profiles, etc.
-## -MACROSS diamonds will be able to determine the capabilities and params of other diamonds, i.e.
-##      "are you a python script" or "can you accept more than one value to search on?"
+## Classify MACROSS tools:
+## -Users get access to their intended toolsets; admin tools won't be copied to user profiles,
+##  tier 3 diamonds won't get copied to tier 1 profiles, etc.
+## -MACROSS diamonds will be able to determine the capabilities and params of other scripts, i.e.
+##  "are you a python script" or "can you accept more than one value to search on?"
 ## ======================================================================================================
 ##                                              IMPORTANT
 ## Custom scripts in the diamonds\ folder need to have the $priv thru $rtype values in a comma-
-## separated line (in-order!) beginning with "#_class " in line 3. Do not leave any field empty!
-## The remaining class fields are all automatically assigned within MACROSS ($fname, $pos, etc.)
-## Make sure to write your $valtypes concise but distinct, example: many diamonds might contain "IP" in their
+## separated line (in-order!) beginning with "#_class " in line 3.
+## Make sure to write your $valtypes concise but distinct, example: many scripts might contain "IP" in their
 ## $valtypes, so differentiate them like "threat IP lookup" vs. "local IP search", etc.
 ## ======================================================================================================
 class macross {
 
-    [string]$name     ## Common name of the diamond
-    [string]$priv     ## *Lowest* privilege level required, admin vs. user; diamonds can contain admin-only functions while still mostly working in userland
-    [string]$access   ## The diamond's intended group: common (everybody) vs. tier1 vs. tier2 vs. tier3
-    [string]$valtype  ## The type of data a diamond is meant to process (IP, strings, logs, etc)
-    [string]$lang     ## The diamond language, powershell vs. python
+    [string]$name     ## Common name of the script
+    [string]$priv     ## *Lowest* privilege level required, admin vs. user; scripts can contain admin-only functions while still mostly working in userland
+    [string]$access   ## The script's intended analyst: common (everybody) vs. tiers 1-3
+    [string]$valtype  ## The type of data a script is meant to process (IP, strings, logs, etc)
+    [string]$lang     ## The script language, powershell vs. python
     [string]$author   ## Script author
-    [int]$evalmax     ## How many values a diamond can accept; max is 2 if you intend to use the macross.collab function!
-    [string]$rtype    ## What kind of response your diamond generates; example types are 'json', 'file', 'string' or 'none'
-    [string]$ver      ## The diamond version
-    [string]$fname    ## The diamond's full filename + extension
-    [string]$desc     ## Description from the diamond's #_sdf1 line
-    [int]$pos         ## Position in the MACROSS menu; used for selection. This value dynamically changes when diamonds are added/removed.
+    [int]$evalmax     ## How many values a script can accept; max is 2 if you intend to use the macross.collab function!
+    [string]$rtype    ## What kind of response your script generates; example types are 'onscreen', 'json', 'file', 'string' or leave empty
+    [string]$ver      ## The script version
+    [string]$fname    ## The script's full filename + extension
+    [string]$desc     ## Description from the script's #_SDF1 line
+    [int]$pos         ## Position in the MACROSS menu; used for selection. This value dynamically changes when scripts are added/removed.
                       ## (Scripts are passed in alpha order)
 
-    <#
-    ** Make sure to be distinct and consistent when assigning an attribute to your diamonds, so that
-    the findDF function gives you the correct diamonds every time! For example, if there are diamonds
-    that return streamed json vs. writing json to a file, you should use separate .rtypes, like 'json'
-    and 'json file'.
-    #>
+    macross($scriptvalues){ $this.classifyScript($scriptvalues) }
 
-    macross($scriptvalues){ $this.classifyDiamond($scriptvalues) }
-
-    ## The diamondCount function in updates.ps1 will feed diamond values here;
+    ## The toolCount function in updates.ps1 will feed script values here;
     ## Once the macross object has been created, it will be stored in the global
-    ## dictionary $dyrl_ATTS to be used by functions that control RBAC and 
+    ## dictionary $dyrl_ATTS to be used by functions that control RBAC and
     ## collaborations
-    [void]classifyDiamond($scriptvalues){
+    [void]classifyScript($scriptvalues){
         $this.name    = ($scriptvalues -Split ',')[0]
         $this.priv    = ($scriptvalues -Split ',')[1]
         $this.access  = ($scriptvalues -Split ',')[2]
@@ -62,8 +50,8 @@ class macross {
         $this.pos     = ($scriptvalues -Split ',')[11]
     }
 
-    ## Attributes are collected automatically, shouldn't need the manual process but it's here for funsies
-    [void]classifyDiamond(
+    ## Attributes are collected automatically, shouldn't need the manual process but who knows
+    [void]classifyScript(
             [string]$name,
             [string]$priv,
             [string]$access,
@@ -91,7 +79,7 @@ class macross {
     }
 
 
-    ## Method checks to automatically divvy out the diamonds by user role or valkyrie requirement
+    ## Method checks to automatically divvy out the tools by user role
     [string]toolInfo(){
         $nm = ' MACROSS: ' + $this.name
         $ve = ' Version:                     ' + $this.ver
@@ -119,7 +107,8 @@ class macross {
         "
         Return $info
     }
-    
+
 }
+
 
 
