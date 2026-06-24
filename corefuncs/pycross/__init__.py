bbc3479a048bad4dd4faffd706a0ef8934a0023f
hisurfadvisory <105613794+hisurfadvisory@users.noreply.github.com>
2026-06-24 17:10:59 -0500

    Update __init__.py

--- corefuncs/pycross/__init__.py
+++ corefuncs/pycross/__init__.py
@@ -1,18 +1,62 @@
-""" MACROSS v2.0 python conversion of MACROSS powershell utilities
+""" MACROSS ce v1.0 python conversion of MACROSS powershell utilities
 
  This library imports a lot of common functions, so I just make them available here
  to avoid having to code multiple imports into a script. i.e. instead of importing
- libs like re, os or datetime, you might be able to just use macross.rgx(), 
- macross.psc() or macross.cdate() to do the basics.
-
- This library was written and tested with Python 3.13
+ libs like re, os or datetime, you might be able to just use macross.rgx(),
+ macross.psc(), macross.cdate(), etc. to do the basics.
 
  CLASSES
 
     macross
-        Applies MACROSS properties to each hunter script
+        Applies MACROSS properties to each diamond script
+
+ DATA
+    PROTOCULTURE
+        The investigation value for diamond scripts to process
+
+    CALLER
+        The name of the diamond script querying other diamonds
+
+    DESKTOP
+        The path to your desktop
+
+    PSVER
+        The major version of powershell currently in use
+
+    USR
+        The logged in user
+
+    ROBOTECH
+        If true, the user doesn't have elevated system/network privilege
+
+    HELP
+        Check if user is requesting to view a diamond's help message
+
+    GBIO
+        Path to the local garbage_io folder that contains the
+        PROTOCULTURE.mori file
+
+    OUTFILES
+        Path to the local MACROSS output folder
+
+    PLUGINS
+        Path to the local MACROSS plugins folder
+
+    RESOURCES
+        Path to the local MACROSS resources folder
+
+    CONTENT
+        Path to the remote MACROSS resources folder (default will be
+        the same as RESOURCES if not configured)
+
+    LATTS
+        A dictionary of all local diamonds and their macross class attributes
+
 
  FUNCTIONS
+
+    collab():
+        sends your data to a MACROSS diamond
 
     cdate():
         get the current date + time
@@ -23,8 +67,8 @@
     errLog():
         write to a MACROSS error log
 
-    findDF():
-        find relevant MACROSS hunters to process your data
+    availableTypes():
+        find relevant MACROSS diamonds to process your data
 
     getFile():
         open a dialog to select files or folders
@@ -35,13 +79,13 @@
     minmay():
         display a terminator image.
 
-    gerwalk():
+    kawamori():
         decrypt a MACROSS data key
 
     psc():
         execute a system command
 
-    reString():
+    gerwalk():
         decode/encode base64 or hexadecimal
 
     rgx():
@@ -50,57 +94,54 @@
     screenResults():
         format blocks of text into a table of up to 3 columns
 
-    battroid():
+    skyWriter():
         generate ascii-art of large block words
 
     slp():
         sleep for n seconds/milliseconds
-    
+
     w():
         onscreen text formatting
-
-    valkyrie():
-        sends your data to a MACROSS diamond 
-
 
 
 """
 
 from .macross import (
-    battroid,
+    availableTypes,
     cdate,
+    collab,
     delfile,
     errLog,
-    findDF,
     getFile,
-    ispath,
-    macross,
-    minmay,
     gerwalk,
+    ispath,
+    minmay,
+    kawamori,
     psc,
-    reString,
     rgx,
     screenResults,
+    skyWriter,
     slp,
     w,
-    valkyrie,
     CALLER,
     CONF,
     DESKTOP,
     GBIO,
     HELP,
-    MODS,
+    DIAMONDS,
     LATTS,
     LOGS,
-    MACROOT,
     N_,
+    ROBOTECH,
     OUTFILES,
     PLUGINS,
-    PROTOCULTURE,
     PSVER,
     RESOURCES,
-    ROBOTECH,
+    MACROOT,
+    PROTOCULTURE,
     TMP,
     USR
 )
+
+
 
